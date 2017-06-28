8eaede71697c632a335bb05c7bd322a9fd9b4bf1
kelber <kelberstuchi@gmail.com>
2017-06-28 16:01:21 -0300

    Update firebaseconfig.ts
    done

--- src/environments/firebaseconfig.ts
+++ src/environments/firebaseconfig.ts
@@ -1,12 +1,5 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyAj-OHBMUdU9W6f--vgTb_h1Q5wzsmUyHY",
-    authDomain: "kelberstuchi-799e9.firebaseapp.com",
-    databaseURL: "https://kelberstuchi-799e9.firebaseio.com",
-    projectId: "kelberstuchi-799e9",
-    storageBucket: "kelberstuchi-799e9.appspot.com",
-    messagingSenderId: "376702750307"
-
-
+   
 };
 firebase.initializeApp(firebaseConfig);
 
